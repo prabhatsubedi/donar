f84ed9a88a829647d47460fdb0bcbf016f86a243
Narayan Subedi <prabhat.aurora@gmail.com>
2017-06-25 16:52:49 +0545

    git ignore

--- build/resources/main/spring/resources.groovy
+++
@@ -1,9 +0,0 @@
-// Place your Spring DSL code here
-import donar.SecurityConfiguration
-import donar.GormUserDetailsService
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-beans = {
-    webSecurityConfiguration(SecurityConfiguration)
-    passwordEncoder(BCryptPasswordEncoder)
-    userDetailsService(GormUserDetailsService)
-}
